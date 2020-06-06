d6a33765d6cde6db4967b2663283e811c22ef06c
Toshiyuki FUJITA <kalibora@gmail.com>
2020-06-06 17:46:34 +0900

    more test

--- tests/Rules/ValidateFieldComparisonCallRuleTest.php
+++ tests/Rules/ValidateFieldComparisonCallRuleTest.php
@@ -31,15 +31,27 @@ class ValidateFieldComparisonCallRuleTest extends RuleTestCase
             [
                 [
                     'Otobank\PHPStan\Doctrine\Rules\Asset\AcmeEntity::$bar accessor is missing',
-                    16,
+                    17,
+                ],
+                [
+                    'Otobank\PHPStan\Doctrine\Rules\Asset\AcmeEntity::$bar accessor is missing',
+                    18,
                 ],
                 [
                     'Otobank\PHPStan\Doctrine\Rules\Asset\AcmeEntity::$baz field is missing',
-                    22,
+                    24,
                 ],
                 [
                     'Otobank\PHPStan\Doctrine\Rules\Asset\AcmeEntity::$baz accessor is missing',
-                    22,
+                    24,
+                ],
+                [
+                    'Otobank\PHPStan\Doctrine\Rules\Asset\AcmeEntity::$baz field is missing',
+                    25,
+                ],
+                [
+                    'Otobank\PHPStan\Doctrine\Rules\Asset\AcmeEntity::$baz accessor is missing',
+                    25,
                 ],
             ]
         );
