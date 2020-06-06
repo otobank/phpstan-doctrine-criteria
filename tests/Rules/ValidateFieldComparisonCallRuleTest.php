0f5eb250b0373c171fd5fd691682c2ab9e469f44
Toshiyuki FUJITA <kalibora@gmail.com>
2020-06-06 12:50:48 +0900

    fix test

--- tests/Rules/ValidateFieldComparisonCallRuleTest.php
+++ tests/Rules/ValidateFieldComparisonCallRuleTest.php
@@ -14,6 +14,7 @@ class ValidateFieldComparisonCallRuleTest extends RuleTestCase
     public function getRule() : Rule
     {
         $objectMetadataResolver = new ObjectMetadataResolver(
+            $this->createReflectionProvider(),
             __DIR__ . '/Asset/objectManagerLoader.php',
             null
         );
